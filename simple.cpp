ffcb46707ee7ad9c66ed67c3843e1ce0964579a1
clairedb <claire.deberny@essca.eu>
2022-06-29 10:21:14 +0200

    simple

--- simple.cpp
+++ simple.cpp
@@ -3,13 +3,15 @@
 #include <string.h>
 using namespace std;
 #define MIN(x, y) ((x) < (y) ? (x) : (y)) // calculate minimum between two values
-int main()
+int main(int ac, char **av)
 {
+    if (ac != 3)
+        return 1;
     int i, j, l1, l2, t, track;
     int dist[50][50];
     // take the strings as input
-    char s1[] = "tutorials";
-    char s2[] = "point";
+    char *s1 = av[1];
+    char *s2 = av[2];
     // stores the lenght of strings s1 and s2
     l1 = strlen(s1);
     l2 = strlen(s2);
@@ -33,6 +35,11 @@ int main()
             {
                 track = 1;
             }
+            // if (strcmp(&s1[i], &s2[j]) == 0)
+            // {
+            //     cout << "The Levinstein distance is:" << dist[i - 1][j];
+            //     return 0;
+            // }
             t = MIN((dist[i - 1][j] + 1), (dist[i][j - 1] + 1));
             dist[i][j] = MIN(t, (dist[i - 1][j - 1] + track));
         }
